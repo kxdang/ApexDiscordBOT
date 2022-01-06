ab89f1a3475f8d433dcaa723d09ce204c80d6715
unknown <ikien.dang@gmail.com>
2022-01-05 20:00:23 -0500

    refactor and use dayjs

--- bot.js
+++ bot.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const axios = require("axios");
 const client = new Discord.Client();
-const moment = require("moment");
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 let globalResponseData;
 
@@ -52,119 +57,93 @@ client.on("message", (msg) => {
             readableDate_end: nextMapEnd,
           } = res?.data?.battle_royale?.next;
 
-          console.log(nextMapStart);
+          const mapsData = {
+            worldsEdge: {
+              colour: "#dd701d",
+              imgUrl:
+                "https://ik.imagekit.io/afkmedia/media/images/27408-3927578639f99991b0b2e9e8d337fe4d.jpeg",
+            },
+            olympus: {
+              colour: "#0c95d4",
+              imgUrl: "https://i.imgur.com/L5McL2d.png",
+            },
+            kingsCanyon: {
+              colour: "#FFEE58",
+              imgUrl:
+                "https://cdn1.dotesports.com/wp-content/uploads/2019/10/08161402/ApexLegends.jpg",
+            },
+            stormPoint: {
+              colour: "#50718C",
+              imgUrl:
+                "https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/storm-point/map-blog-thumbnail.png.adapt.1920w.png",
+            },
+            default: {
+              colour: "#E99094",
+              imgUrl:
+                "https://preview.redd.it/jpxsjxwycf541.jpg?width=960&crop=smart&auto=webp&s=5a16d60576ca9abec9d3074ed5acf74b0d9c5019",
+            },
+          };
+
+          const generateEmbeddedMsg = (colour, image) =>
+            new Discord.MessageEmbed()
+              .setColor(colour)
+              .setImage(image)
+              .setDescription(
+                `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${dayjs(
+                  nextMapStart
+                )
+                  .utcOffset(11)
+                  .format("h:mm A")} - ${dayjs(nextMapEnd)
+                  .utcOffset(11)
+                  .format("h:mm A")} **(PST)** \n Time: ${dayjs(nextMapStart)
+                  .utcOffset(14)
+                  .format("h:mm A")} - ${dayjs(nextMapEnd)
+                  .utcOffset(14)
+                  .format("h:mm A")} **(EST)** `
+              );
+
           client.user.setActivity(`${currentMap} (${remainingTimer})`);
-
-          const olympusMessage = new Discord.MessageEmbed()
-            .setColor("#0c95d4")
-            .setImage("https://i.imgur.com/L5McL2d.png")
-            .setDescription(
-              `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${moment(
-                nextMapStart
-              )
-                .add(17, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(17, "h")
-                .format("h:mm A")} **(PST)** \n Time: ${moment(nextMapStart)
-                .add(20, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(20, "h")
-                .format("h:mm A")} **(EST)** `
-            );
-
-          const worldsEdgeMessage = new Discord.MessageEmbed()
-            .setColor("#dd701d")
-            .setImage(
-              "https://ik.imagekit.io/afkmedia/media/images/27408-3927578639f99991b0b2e9e8d337fe4d.jpeg"
-            )
-            .setDescription(
-              `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${moment(
-                nextMapStart
-              )
-                .add(17, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(17, "h")
-                .format("h:mm A")} **(PST)** \n Time: ${moment(nextMapStart)
-                .add(20, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(20, "h")
-                .format("h:mm A")} **(EST)** `
-            );
-
-          const kingsCanyonMessage = new Discord.MessageEmbed()
-            .setColor("#FFEE58")
-            .setImage(
-              "https://cdn1.dotesports.com/wp-content/uploads/2019/10/08161402/ApexLegends.jpg"
-            )
-            .setDescription(
-              `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${moment(
-                nextMapStart
-              )
-                .add(17, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(17, "h")
-                .format("h:mm A")} **(PST)** \n Time: ${moment(nextMapStart)
-                .add(20, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(20, "h")
-                .format("h:mm A")} **(EST)** `
-            );
-
-          const stormPointMsg = new Discord.MessageEmbed()
-            .setColor("#50718C")
-            .setImage(
-              "https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/storm-point/map-blog-thumbnail.png.adapt.1920w.png"
-            )
-            .setDescription(
-              `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${moment(
-                nextMapStart
-              )
-                .add(17, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(17, "h")
-                .format("h:mm A")} **(PST)** \n Time: ${moment(nextMapStart)
-                .add(20, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(20, "h")
-                .format("h:mm A")} **(EST)** `
-            );
-
-          const defaultMsg = new Discord.MessageEmbed()
-            .setColor("#E99094")
-            .setImage(
-              "https://preview.redd.it/jpxsjxwycf541.jpg?width=960&crop=smart&auto=webp&s=5a16d60576ca9abec9d3074ed5acf74b0d9c5019"
-            )
-            .setDescription(
-              `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n Time: ${moment(
-                nextMapStart
-              )
-                .add(17, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(17, "h")
-                .format("h:mm A")} **(PST)** \n Time: ${moment(nextMapStart)
-                .add(20, "h")
-                .format("h:mm A")} - ${moment(nextMapEnd)
-                .add(20, "h")
-                .format(
-                  "h:mm A"
-                )} **(EST)** \n **KIEN FIX IMAGE - APEX UPDATED !!!!**`
-            );
 
           switch (currentMap) {
             case "World's Edge":
-              msg.reply(worldsEdgeMessage);
+              msg.reply(
+                generateEmbeddedMsg(
+                  mapsData.worldsEdge.colour,
+                  mapsData.worldsEdge.imgUrl
+                )
+              );
               break;
             case "Olympus":
-              msg.reply(olympusMessage);
+              msg.reply(
+                generateEmbeddedMsg(
+                  mapsData.olympus.colour,
+                  mapsData.olympus.imgUrl
+                )
+              );
               break;
             case "Kings Canyon":
-              msg.reply(kingsCanyonMessage);
+              msg.reply(
+                generateEmbeddedMsg(
+                  mapsData.kingsCanyon.colour,
+                  mapsData.kingsCanyon.imgUrl
+                )
+              );
               break;
             case "Storm Point":
-              msg.reply(stormPointMsg);
+              msg.reply(
+                generateEmbeddedMsg(
+                  mapsData.stormPoint.colour,
+                  mapsData.stormPoint.imgUrl
+                )
+              );
               break;
             default:
-              msg.reply(defaultMsg);
+              msg.reply(
+                generateEmbeddedMsg(
+                  mapsData.default.colour,
+                  mapsData.default.imgUrl
+                )
+              );
           }
         });
     } catch (e) {

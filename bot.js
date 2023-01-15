e37a799f59a5ed2d72e5e3b86921017265d939f0
unknown <ikien.dang@gmail.com>
2023-01-15 12:12:09 -0500

    fixing utc conversion

--- bot.js
+++ bot.js
@@ -40,6 +40,38 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
+  const mapsData = {
+    brokenMoon: {
+      colour: "##0a16fa",
+      imgUrl:
+        "https://cdn1.dotesports.com/wp-content/uploads/2022/10/20144307/Apex-Legends-Broken-Moon-Powerizer-1024x576.jpeg",
+    },
+    worldsEdge: {
+      colour: "#dd701d",
+      imgUrl:
+        "https://ik.imagekit.io/afkmedia/media/images/27408-3927578639f99991b0b2e9e8d337fe4d.jpeg",
+    },
+    olympus: {
+      colour: "#0c95d4",
+      imgUrl: "https://i.imgur.com/L5McL2d.png",
+    },
+    kingsCanyon: {
+      colour: "#FFEE58",
+      imgUrl:
+        "https://cdn1.dotesports.com/wp-content/uploads/2019/10/08161402/ApexLegends.jpg",
+    },
+    stormPoint: {
+      colour: "#50718C",
+      imgUrl:
+        "https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/storm-point/map-blog-thumbnail.png.adapt.1920w.png",
+    },
+    default: {
+      colour: "#E99094",
+      imgUrl:
+        "https://preview.redd.it/jpxsjxwycf541.jpg?width=960&crop=smart&auto=webp&s=5a16d60576ca9abec9d3074ed5acf74b0d9c5019",
+    },
+  };
+
   if (msg.content.toLowerCase() === "!map") {
     try {
       axios
@@ -48,45 +80,14 @@ client.on("message", (msg) => {
         )
         .then((res) => {
           globalResponseData = res;
-
           const { map: currentMap, remainingTimer } =
             res?.data?.battle_royale?.current;
+
           const {
             map: nextMap,
             readableDate_start: nextMapStart,
             readableDate_end: nextMapEnd,
           } = res?.data?.battle_royale?.next;
-          const mapsData = {
-            brokenMoon: {
-              colour: "##0a16fa",
-              imgUrl:
-                "https://cdn1.dotesports.com/wp-content/uploads/2022/10/20144307/Apex-Legends-Broken-Moon-Powerizer-1024x576.jpeg",
-            },
-            worldsEdge: {
-              colour: "#dd701d",
-              imgUrl:
-                "https://ik.imagekit.io/afkmedia/media/images/27408-3927578639f99991b0b2e9e8d337fe4d.jpeg",
-            },
-            olympus: {
-              colour: "#0c95d4",
-              imgUrl: "https://i.imgur.com/L5McL2d.png",
-            },
-            kingsCanyon: {
-              colour: "#FFEE58",
-              imgUrl:
-                "https://cdn1.dotesports.com/wp-content/uploads/2019/10/08161402/ApexLegends.jpg",
-            },
-            stormPoint: {
-              colour: "#50718C",
-              imgUrl:
-                "https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/storm-point/map-blog-thumbnail.png.adapt.1920w.png",
-            },
-            default: {
-              colour: "#E99094",
-              imgUrl:
-                "https://preview.redd.it/jpxsjxwycf541.jpg?width=960&crop=smart&auto=webp&s=5a16d60576ca9abec9d3074ed5acf74b0d9c5019",
-            },
-          };
 
           const generateEmbeddedMsg = (colour, image) =>
             new Discord.MessageEmbed()
@@ -96,15 +97,23 @@ client.on("message", (msg) => {
                 `**CURRENT MAP**: ${currentMap}\n > Time Remaining: ${remainingTimer} \n **NEXT MAP**: ${nextMap} \n **Time in (PST)**: ${dayjs(
                   nextMapStart
                 )
-                  .utcOffset(13)
+                  .utc("z")
+                  .local()
+                  .tz("America/Vancouver")
                   .format("h:mm A")} - ${dayjs(nextMapEnd)
-                  .utcOffset(13)
+                  .utc("z")
+                  .local()
+                  .tz("America/Vancouver")
                   .format("h:mm A")}  \n **Time in (EST)**: ${dayjs(
                   nextMapStart
                 )
-                  .utcOffset(16)
+                  .utc("z")
+                  .local()
+                  .tz("America/Toronto")
                   .format("h:mm A")} - ${dayjs(nextMapEnd)
-                  .utcOffset(16)
+                  .utc("z")
+                  .local()
+                  .tz("America/Toronto")
                   .format("h:mm A")}  `
               );
 
